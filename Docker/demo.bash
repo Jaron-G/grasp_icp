3d7080033a792f9dc673efe23c311d15f338d0f8
GZ <87937314+Jaron-G@users.noreply.github.com>
2024-06-08 00:53:21 +0800

    Update demo.bash

--- Docker/demo.bash
+++ Docker/demo.bash
@@ -6,20 +6,10 @@ docker run --rm -it \
     --env="QT_X11_NO_MITSHM=1" \
     --env="XAUTHORITY=$XAUTH" \
     --volume="/home/$USER/catkin_ws/src/grasp_icp:/catkin_ws/src/grasp_icp" \
-    --volume="/home/$USER/catkin_ws/src/s_s_ros/src/pkg_gripper:/catkin_ws/src/pkg_gripper" \
-    --volume="/home/$USER/catkin_ws/src/s_s_ros/src/pkg_ur10e_gripper:/catkin_ws/src/pkg_ur10e_gripper" \
-    --volume="/home/$USER/catkin_ws/src/s_s_ros/src/pkg_ur10e_gripper_moveit:/catkin_ws/src/pkg_ur10e_gripper_moveit" \
-    --volume="/home/$USER/catkin_ws/src/s_s_ros/src/models:/catkin_ws/src/models" \
-    --volume="/home/$USER/catkin_ws/src/gazebo-pkgs:/catkin_ws/src/gazebo-pkgs" \
-    --volume="/home/$USER/catkin_ws/src/general-message-pkgs:/catkin_ws/src/general-message-pkgs" \
-    --volume="/home/$USER/catkin_ws/src/halcon_package:/catkin_ws/src/halcon_package" \
-    --volume="/home/$USER/catkin_ws/src/pose_transformation:/catkin_ws/src/pose_transformation" \
-    --volume="/home/$USER/catkin_ws/src/obtain_pcd:/catkin_ws/src/obtain_pcd" \
-    --volume="/home/$USER/catkin_ws/src/move_robot:/catkin_ws/src/move_robot" \
     --volume="/dev/bus/usb:/dev/bus/usb" \
     --net=host \
     --privileged \
-    ros-noetic-grasp-sim \
+    ros-noetic-industrial-grasp-demo \
     bash
 
 echo "Done."

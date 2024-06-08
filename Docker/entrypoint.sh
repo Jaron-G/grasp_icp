1512a851e7065eb0143c3962e47dca55bd126f3e
GZ <2205250117@qq.com>
2024-06-08 20:05:49 +0800

    Add entrypoint for docker

---
+++ Docker/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# entrypoint.sh
+
+# 输出正在开始同步仓库的信息
+echo "Starting synchronization of Git repositories..."
+echo "-----------------------------------------------"
+
+# Update repositories
+cd /catkin_ws/src/robotiq_gripper && git pull
+cd /catkin_ws/src/ur10e_gripper && git pull
+cd /catkin_ws/src/ur10e_gripper_moveit && git pull
+cd /catkin_ws/src/models && git pull
+cd /catkin_ws/src/obtain_pcd && git pull
+cd /catkin_ws/src/pose_transformation && git pull
+cd /catkin_ws/src/move_robot && git pull
+cd /catkin_ws/src/halcon_package && git pull
+
+# 输出完成同步的信息
+echo "-----------------------------------------------"
+echo "Synchronization of all repositories completed."
+
+# 移动到工作空间文件夹
+cd /catkin_ws
+
+# 执行容器的主命令
+exec "$@"

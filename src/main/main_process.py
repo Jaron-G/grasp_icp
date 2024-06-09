73b76cd91d2b11d6b093b89fdcc38b0953ec6228
GZ <2205250117@qq.com>
2024-06-09 19:32:49 +0800

    为主程序添加GUI界面

--- src/main/main_process.py
+++ src/main/main_process.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import rospy
-import keyboard
+import tkinter as tk
 
 from obtain_pcd.srv import ObtainPcd
 from halcon_package.srv import RegistratePose
@@ -14,7 +14,7 @@ def obtain_pcd_client():
         resp1 = obtain_pcd()
         return resp1
     except rospy.ServiceException as e:
-        print("Service call failed: %s"%e)
+        print("Service call failed: %s" % e)
 
 def registration_client():
     rospy.wait_for_service('registration')
@@ -23,7 +23,7 @@ def registration_client():
         resp1 = registration()
         return resp1
     except rospy.ServiceException as e:
-        rospy.logerr("Service call failed: %s"%e)
+        rospy.logerr("Service call failed: %s" % e)
 
 def pose_transformation_client(x, y):
     rospy.wait_for_service('pose_transformation')
@@ -32,7 +32,7 @@ def pose_transformation_client(x, y):
         resp1 = pose_transformation(x, y)
         return resp1
     except rospy.ServiceException as e:
-        print("Service call failed: %s"%e)
+        print("Service call failed: %s" % e)
 
 def move_robot_to_pose_client(x, y, z):
     rospy.wait_for_service('move_to_pose_service')
@@ -41,35 +41,88 @@ def move_robot_to_pose_client(x, y, z):
         resp1 = move_to_pose(x, y, z)
         return resp1.success
     except rospy.ServiceException as e:
-        rospy.logerr("Service call failed: %s"%e)
+        rospy.logerr("Service call failed: %s" % e)
 
-def main_process():
-    while not rospy.is_shutdown():
-        if  keyboard.is_pressed('1'):
-            rospy.loginfo("Start btain pointcloud !")
-            response = obtain_pcd_client()
-            rospy.loginfo("Obtain pointcloud completed!")
-        elif keyboard.is_pressed('2'):
-            rospy.loginfo("Start perform registration !")
-            response = registration_client()
-            final_pose, matched_model = response.final_pose,response.matched_model
-            rospy.loginfo("Registration completed !")
-        elif keyboard.is_pressed('3'):
-            rospy.loginfo("Pose transformation !")
-            response = pose_transformation_client(final_pose, matched_model)
-            rotation_matrix,pose_g, pose_up = response.rotation_matrix, response.grasp_pose, response.up_pose
-            rospy.loginfo("Pose transformation completed !")
-        elif keyboard.is_pressed('4'):
-            rospy.loginfo("Start grasp !")
-            result = move_robot_to_pose_client(rotation_matrix, pose_g, pose_up)
-            if result:
-                rospy.loginfo("Robot moved successfully!")
-            else:
-                rospy.loginfo("Failed to move robot.")
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+        self.matched_model = None
+        self.final_pose = None
+        self.pose_up = None
+        self.pose_g = None
+        self.rotation_matrix = None
+
+    def create_widgets(self):
+        self.frame1 = tk.Frame(self)
+        self.frame2 = tk.Frame(self)
+        self.obtain_pcd_button = tk.Button(self.frame1, text="Get scene point cloud", command=self.press_obtain_pcd_button,width=28
+                                           # , height=2,font=32
+                                           )
+        self.obtain_pcd_button.pack(pady=5)
+        self.registration_button = tk.Button(self.frame1, text="Perform registration", command=self.press_registration_button,width=28)
+        self.registration_button.pack(pady=5)
+        self.pose_transformation_button = tk.Button(self.frame1, text="Perform pose transformation", command=self.press_pose_transformation_button,width=28)
+        self.pose_transformation_button.pack(pady=5)
+        self.move_robot_button = tk.Button(self.frame1, text="Start grasp", command=self.press_move_robot_button,width=28)
+        self.move_robot_button.pack(pady=5)
+        self.quit = tk.Button(self.frame1, text="QUIT", fg="red", command=self.master.destroy, bd=3,width=28)
+        self.quit.pack(pady=5)
+
+        self.photo = tk.PhotoImage(file="/catkin_ws/src/grasp_icp/src/main/robot.png")
+        self.photo_label = tk.Label(self.frame2,justify = tk.LEFT,image = self.photo)
+        self.photo_label.pack(side="right")
+
+        self.message_var = tk.StringVar(self,value='Please click the buttons in order !')  # 储存文字的类
+        self.message_box = tk.Label(self.frame1, textvariable=self.message_var, bg='lightblue',width=28)
+        self.message_box.pack(pady=5)
+
+        self.frame1.pack(side="left")  # 左框架对齐
+        self.frame2.pack(side="right")  # 右框架对齐
+
+    def press_obtain_pcd_button(self):
+        print("Start obtain point cloud !")
+        rospy.loginfo("Start obtain point cloud !")
+        response = obtain_pcd_client()
+        rospy.loginfo("Obtain point cloud completed!")
+        self.message_var.set("Obtain point cloud completed!")
+
+    def press_registration_button(self):
+        print("Start perform registration !")
+        rospy.loginfo("Start perform registration !")
+        response = registration_client()
+        self.final_pose, self.matched_model = response.final_pose, response.matched_model
+        rospy.loginfo("Registration completed !")
+        self.message_var.set("Registration completed !")
+
+    def press_pose_transformation_button(self):
+        print("Pose transformation !")
+        rospy.loginfo("Pose transformation !")
+        response = pose_transformation_client(self.final_pose, self.matched_model)
+        self.rotation_matrix, self.pose_g, self.pose_up = response.rotation_matrix, response.grasp_pose, response.up_pose
+        rospy.loginfo("Pose transformation completed !")
+        self.message_var.set("Pose transformation completed !")
+
+    def press_move_robot_button(self):
+        print("Start grasp !")
+        rospy.loginfo("Start grasp !")
+        result = move_robot_to_pose_client(self.rotation_matrix, self.pose_g, self.pose_up)
+        if result:
+            rospy.loginfo("Robot moved successfully!")
         else:
-            pass
-        
+            rospy.loginfo("Failed to move robot.")
+        self.message_var.set("Robot moved successfully!")
+
+def main():
+    root = tk.Tk()
+    root.title('Pipe grasping based on ICP registration')
+    root.geometry('400x240')
+    app = Application(master=root)
+    app.mainloop()
+
 if __name__ == '__main__':
     rospy.init_node('main_process')
-    main_process()
+    main()
     rospy.spin()
